62d49984c9497d5ee7e420b72ff2a959abe3e957
Leonid <peretolcinleonid4@gmail.com>
2025-04-30 19:18:54 +0300

    connect new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnotherTestComponentComponent } from './another-test-component/another-test-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    AnotherTestComponentComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
