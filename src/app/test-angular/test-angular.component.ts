b550b3e76821500dd5f18af96c672653ed51e3ca
Leonid <peretolcinleonid4@gmail.com>
2025-04-30 19:40:40 +0300

    create new component testAngular

---
+++ src/app/test-angular/test-angular.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-angular',
+  imports: [],
+  templateUrl: './test-angular.component.html',
+  styleUrl: './test-angular.component.css'
+})
+export class TestAngularComponent {
+
+}
